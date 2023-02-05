d098303209023efc1846def8183a672ee8209fad
Димитър Янакиев <dyanakiev@codingburgas.bg>
2023-02-05 23:49:10 +0200

    Initial commit

---
+++ Project files/2223-sd-three-layer-Hotel-management-system-CSharp/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace _2223_sd_three_layer_Hotel_management_system_CSharp_solution
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ Project files/Start/Start.cs
@@ -0,0 +1,10 @@
+﻿namespace Start
+{
+    internal class Start
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
